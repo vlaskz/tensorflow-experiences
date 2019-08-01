fa23911f1fc5702459cfbb38771400dc13ff9f06
Sam Velasquez <atrox.lapa@hotmail.com>
2019-07-31 22:13:24 -0300

    feat: create and train model

--- experiences/handwritenDigitRecogWithCNNs/script.js
+++ experiences/handwritenDigitRecogWithCNNs/script.js
@@ -29,5 +29,98 @@ async function run() {
     const data = new MnistData()
     await data.load();
     await showExamples(data)
+    const model = getModel()
+    tfvis.show.modelSummary({ name: 'Model Architecture' }, model)
+    await train(model, data)
 }
+
+function getModel() {
+    const model = tf.sequential()
+
+    const IMAGE_WIDTH = 28
+    const IMAGE_HEIGHT = 28
+    const IMAGE_CHANNELS = 1
+
+    model.add(tf.layers.conv2d({
+        inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+        kernelSize: 5,
+        filters: 8,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+    }))
+
+    model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
+
+    model.add(tf.layers.conv2d({
+        kernelSize: 5,
+        filters: 16,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+    }))
+    
+    model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
+
+    model.add(tf.layers.flatten())
+
+    const NUM_OUTPUT_CLASSES = 10
+
+    model.add(tf.layers.dense({
+        units: NUM_OUTPUT_CLASSES,
+        kernelInitializer: 'varianceScaling',
+        activation: 'softmax'
+    }))
+
+    const optimizer = tf.train.adam()
+
+    model.compile({
+        optimizer: optimizer,
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy']
+    })
+
+    return model
+}
+
+async function train(model, data) {
+
+    const metrics = ['loss', 'val_loss', 'acc', 'val_acc']
+    const container = {
+        name: 'Model Training', styles: { height: '1000px' }
+    }
+
+    const fitCallbacks = tfvis.show.fitCallbacks(container, metrics)
+
+    const BATCH_SIZE = 512
+    const TRAIN_DATA_SIZE = 5500
+    const TEST_DATA_SIZE = 1000
+
+    const [trainXs, trainYs] = tf.tidy(() => {
+        const d = data.nextTrainBatch(TRAIN_DATA_SIZE)
+        return [
+            d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
+            d.labels
+        ]
+    })
+
+    const [testXs, testYs] = tf.tidy(() => {
+        const d = data.nextTestBatch(TEST_DATA_SIZE)
+        return [
+            d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
+            d.labels
+        ]
+    })
+
+    return model.fit(trainXs, trainYs, {
+        batchSize: BATCH_SIZE,
+        validationData: [testXs, testYs],
+        epochs: 10,
+        shuffle: true,
+        callbacks: fitCallbacks
+    })
+}
+
+
+
 document.addEventListener('DOMContentLoaded', run)
